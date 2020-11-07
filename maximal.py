48c1272300a57215aac614e1176392d2651b0098
Kartikeylakhotia14 <38350684+Kartikeylakhotia14@users.noreply.github.com>
2020-11-07 11:38:42 +0530

    Add files via upload

---
+++ maximal.py
@@ -0,0 +1,21 @@
+NO_OF_CHARS = 256
+def longestUniqueSubsttr(string): 
+
+	lastIndex = [-1] * NO_OF_CHARS 
+
+	n = len(string) 
+	res = 0  
+	i = 0
+
+	for j in range(0, n): 
+		i = max(i, lastIndex[ord(string[j])] + 1); 
+
+		res = max(res, j - i + 1) 
+
+		lastIndex[ord(string[j])] = j; 
+
+	return res 
+
+string = input()
+length = longestUniqueSubsttr(string) 
+print (str(length)) 
